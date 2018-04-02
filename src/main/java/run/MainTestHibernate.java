ff5ae334c5765285add6f928d15c266f3fbbf51d
s.garmaev <garmsayan@gmail.com>
2018-04-02 17:53:34 +0300

    First commit

--- src/main/java/run/MainTestHibernate.java
+++ src/main/java/run/MainTestHibernate.java
@@ -6,26 +6,56 @@ import utils.DAOutils;
 import java.util.List;
 
 public class MainTestHibernate {
-    public static DAOutils dao=new DAOutils();
-    public static void main(String[] args){
+    public static DAOutils dao = new DAOutils();
+
+    public static void main(String[] args) {
         OrderHeader orderHeader = new OrderHeader();
         orderHeader.setCustomerName("Gepard4444444");
         orderHeader.setCustomerAddress("Kharkov222222 str.");
 
         dao.addEntity(orderHeader);
 
-        List<OrderHeader> orderHeaders=dao.getListEntity(OrderHeader.class);
+//        READ
+        List<OrderHeader> orderHeaders = dao.getListEntity(OrderHeader.class);
         System.out.println(orderHeader);
+        orderHeaders = dao.geListEntity(OrderHeader.class, "customerName", "Gepard4444444");
 
-
+//        UPDATE
+//      Update via HQL
         dao.updateEntityHQL(OrderHeader.class, "orderID", 83, "customerName", "HibernateTest");
 
-       // orderHeader.setOrderID(82);
-        orderHeader.setCustomerName("XXXXXXXXX");
-        orderHeader.setCustomerAddress("YYYYYYYYYYY");
-        dao.updateEntity(OrderHeader.class, 86);
+        orderHeader.setOrderID(153);
+        orderHeader.setCustomerName("Gepard9999");
+        orderHeader.setCustomerAddress("9999999");
+//        Update via update
+        dao.updateEntity(orderHeader);
 
-        int i=0;
+//        DELETE
+        orderHeader.setOrderID(152);
+        orderHeader.setCustomerName("Gepard4444444");
+        orderHeader.setCustomerAddress("Kharkov222222 str.");
 
+        dao.deleteEntity(orderHeader);
+
+        dao.deleteEntity(OrderHeader.class, "customerName", "Gepard4444444");
+
+        dao.deleteEntity(OrderHeader.class, 31);
+
+        orderHeaders = dao.getListEntity(OrderHeader.class);
+
+//        orderHeaders.forEach(orderHeaderEntity -> {
+//            if (orderHeaderEntity.getCustomerName() != null) {
+//                if (orderHeaderEntity.getCustomerName().contains("8888888888")) {
+//                    dao.deleteEntity(orderHeaderEntity);
+//                }
+//            }
+//        });
+
+        orderHeaders.stream()
+                .filter(orderHeaderEntity -> orderHeaderEntity.getCustomerName() != null
+                        && orderHeaderEntity.getCustomerName().contains("33333"))
+                .forEach(dao::deleteEntity);
     }
+
+
 }
--- src/main/java/utils/DAOutils.java
+++ src/main/java/utils/DAOutils.java
@@ -1,11 +1,13 @@
 package utils;
 
 import models.OrderHeader;
+import org.hibernate.Criteria;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
 
-import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DAOutils {
@@ -21,6 +23,28 @@ public class DAOutils {
             } else {
                 System.out.println("list " + queryList);
                 return (List<T>) queryList;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            session.close();
+        }
+    }
+
+    public <T> List<T> geListEntity(Class<T> type, String column, String value) {
+        List<T> entities = new ArrayList<T>();
+        Session session = null;
+        try {
+            session = SessionHibernate.getSessionFactory().openSession();
+            Criteria userCriteria = session.createCriteria(type);
+            userCriteria.add(Restrictions.eq(column, value));
+            entities = (List<T>) userCriteria.list();
+            if (entities.isEmpty()) {
+                return null;
+            } else {
+                System.out.println("list " + entities);
+                return entities;
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -50,15 +74,15 @@ public class DAOutils {
     public <T> void updateEntityHQL(Class<T> entity, String searchByColumn, Integer searchFor, String updateColumn, String newValue) {
         Session session = null;
         Transaction transaction = null;
-        OrderHeader orderHeader=new OrderHeader();
+        OrderHeader orderHeader = new OrderHeader();
         try {
             session = SessionHibernate.getSessionFactory().openSession();
             transaction = session.beginTransaction();
 //            orderHeader = (OrderHeader) session.get(OrderHeader.class, 80);
-            String hqlUpdate = "update "+entity.getName()+ " set "+updateColumn +"= :updateColumn where "+searchByColumn+" = :searchByColumn";
-            int updatedEntities = session.createQuery( hqlUpdate )
-                    .setString( "updateColumn", newValue)
-                    .setInteger( "searchByColumn", searchFor )
+            String hqlUpdate = "update " + entity.getName() + " set " + updateColumn + "= :updateColumn where " + searchByColumn + " = :searchByColumn";
+            int updatedEntities = session.createQuery(hqlUpdate)
+                    .setString("updateColumn", newValue)
+                    .setInteger("searchByColumn", searchFor)
                     .executeUpdate();
             transaction.commit();
         } catch (Exception e) {
@@ -68,23 +92,73 @@ public class DAOutils {
         }
     }
 
-    public <T> void updateEntity(Class<T> entityType, Object id, T entity) {
+    public <T> void updateEntity(T entity) {
         Session session = null;
-        Transaction transaction = null;
-        OrderHeader orderHeader=new OrderHeader();
+        Transaction transaction;
         try {
             session = SessionHibernate.getSessionFactory().openSession();
             transaction = session.beginTransaction();
-            orderHeader = (OrderHeader) session.get(entityType, (Serializable) id);
-            orderHeader.setCustomerName("OOOOOOOOOOOOO");
-            orderHeader.setCustomerAddress("AAAAAAAAAAAAAAA");
-
-//            session.update(o);
+            session.update(entity);
+//            session.saveOrUpdate(entity);
             transaction.commit();
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             session.close();
+        }
+    }
+
+    public <T> boolean deleteEntity(T entity) {
+        Session session = null;
+        Transaction transaction;
+        try {
+            session = SessionHibernate.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.delete(entity);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            session.close();
+            return true;
+        }
+    }
+
+    public <T> boolean deleteEntity(Class<T> entity, String column, String value) {
+        Session session = null;
+        Transaction transaction;
+        try {
+            session = SessionHibernate.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.createQuery("delete " + entity.getName() + " where " + column + " = :value")
+                    .setParameter("value",value).executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            session.close();
+            return true;
+        }
+    }
+
+    public <T> boolean deleteEntity(Class<T> entity, Integer id) {
+        Session session = null;
+        Transaction transaction;
+        try {
+            session = SessionHibernate.getSessionFactory().openSession();
+            session.beginTransaction();
+            T instance=session.load(entity, id);
+            session.delete(instance);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Ups!!!!!!!!!!!!!!!!!!!!!!!!!1");
+            return false;
+        } finally {
+            session.close();
+            return true;
         }
     }
 
